2fe738803f64dc1378ce8b9e0708fa43609493db
mdepasquale <mdepasquale@cs.stonybrook.edu>
2016-08-30 15:36:37 -0700

    Fixed text.c so it only shows the code from the hw 0 document.

--- hw0/test.c
+++ hw0/test.c
@@ -7,13 +7,3 @@ fprintf(stdout, "%s\n", "Hello Students!");
 return EXIT_SUCCESS;
 }
 
-// hello.c
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	printf("Code pulled from remote repository.\n");
-	return EXIT_SUCCESS;
-}
-
-
