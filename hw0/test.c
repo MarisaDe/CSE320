71552d1723700b5d4a88aabbb3e249fbb5ae6ad6
mdepasquale <mdepasquale@cs.stonybrook.edu>
2016-08-30 15:15:53 -0700

    Copied and pasted the code at the top of HW 0.

--- hw0/test.c
+++ hw0/test.c
@@ -1,3 +1,12 @@
+
+/* Hello Students! program */
+#include <stdlib.h>
+#include <stdio.h>
+int main(int argc, char* argv[]) {
+fprintf(stdout, "%s\n", "Hello Students!");
+return EXIT_SUCCESS;
+}
+
 // hello.c
 #include <stdlib.h>
 #include <stdio.h>
@@ -6,3 +15,5 @@ int main(int argc, char *argv[]) {
 	printf("Code pulled from remote repository.\n");
 	return EXIT_SUCCESS;
 }
+
+
